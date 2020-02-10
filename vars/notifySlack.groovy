f96f5721c633e6f0a8609ca87e34a54a731cf8cb
Julian Barnett <julianb@netapp.com>
2020-02-10 14:45:47 -0700

    first commit

---
+++ vars/notifySlack.groovy
@@ -0,0 +1,75 @@
+/**
+* notify slack and set message based on build status
+*/
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.model.Actionable;
+
+def call(String buildStatus = 'STARTED', String channel = '#info-shared-services-sre') {
+
+  // buildStatus of null means successfull
+  buildStatus = buildStatus ?: 'SUCCESSFUL'
+  channel = channel ?: '#info-shared-services-sre'
+
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] (<${env.RUN_DISPLAY_URL}|Open>) (<${env.RUN_CHANGES_DISPLAY_URL}|  Changes>)'"
+  def title = "${env.JOB_NAME} Build: ${env.BUILD_NUMBER}"
+  def title_link = "${env.RUN_DISPLAY_URL}"
+  def branchName = "${env.GIT_BRANCH}"
+
+  def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+  def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an'").trim()
+
+  def message = sh(returnStdout: true, script: 'git log -1 --pretty=%B').trim()
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = 'good'
+  } else if (buildStatus == 'UNSTABLE') {
+    color = 'YELLOW'
+    colorCode = 'warning'
+  } else {
+    color = 'RED'
+    colorCode = 'danger'
+  }
+
+  JSONObject attachment = new JSONObject();
+  attachment.put('author',"jenkins");
+  attachment.put('title', title.toString());
+  attachment.put('title_link',title_link.toString());
+  attachment.put('text', subject.toString());
+  attachment.put('fallback', "fallback message");
+  attachment.put('color',colorCode);
+  attachment.put('mrkdwn_in', ["fields"])
+  // JSONObject for branch
+  JSONObject branch = new JSONObject();
+  branch.put('title', 'Branch');
+  branch.put('value', branchName.toString());
+  branch.put('short', true);
+  // JSONObject for author
+  JSONObject commitAuthor = new JSONObject();
+  commitAuthor.put('title', 'Author');
+  commitAuthor.put('value', author.toString());
+  commitAuthor.put('short', true);
+  // JSONObject for branch
+  JSONObject commitMessage = new JSONObject();
+  commitMessage.put('title', 'Commit Message');
+  commitMessage.put('value', message.toString());
+  commitMessage.put('short', false);
+  attachment.put('fields', [branch, commitAuthor, commitMessage]);
+  JSONArray attachments = new JSONArray();
+  attachments.add(attachment);
+  println attachments.toString()
+
+  // Send notifications
+  slackSend (color: colorCode, message: subject, attachments: attachments.toString(), channel: channel)
+
+}
